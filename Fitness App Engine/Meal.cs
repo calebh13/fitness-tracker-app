e12b47e5bb504fbb3914cc2dd6e0bdeba927fa7e
afischa <asafischer42@gmail.com>
2025-04-01 16:19:06 -0700

    meals class

---
+++ Fitness App Engine/Meal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_App_Engine
+{
+    class Meal
+    {
+        private string food;
+        private int calories;
+        private int protein;
+
+        public string Food {  get { return food; } set { food = value; } }
+        public int Protein { get { return protein; } set { protein = value; } }
+        private int Calories { get { return calories; } set { calories = value; } }
+        
+        public Meal(string food, int calories, int protein)
+        {
+            this.food = food;
+            this.calories = calories;
+            this.protein = protein;
+        }
+    }
+}
