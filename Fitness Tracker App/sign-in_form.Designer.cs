516fa540ce691582151a2e55840776b7ee5d6c54
JamesRtoP <yoilbob@outlook.com>
2025-04-20 13:33:18 -0700

    Need to update to main

---
+++ Fitness Tracker App/sign-in_form.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Fitness_Tracker_App
+{
+    partial class sign_in_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            Title = new Label();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(340, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(0, 39);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Title
+            // 
+            Title.AutoSize = true;
+            Title.Font = new Font("Segoe UI", 25F);
+            Title.Location = new Point(346, 47);
+            Title.Name = "Title";
+            Title.Size = new Size(425, 89);
+            Title.TabIndex = 1;
+            Title.Text = "Select A User";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(246, 405);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(376, 40);
+            comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1174, 1170);
+            Controls.Add(comboBox1);
+            Controls.Add(Title);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label Title;
+        private ComboBox comboBox1;
+    }
+}
